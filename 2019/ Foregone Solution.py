d6f17b1141cd5c5fe64e5b45edfd197ed57eaafe
simone patuelli <simone.patuelli@mail.polimi.it>
2019-04-07 10:08:31 +0200

    Final Submission

--- 2019/ Foregone Solution.py
+++ 2019/ Foregone Solution.py
@@ -1,7 +1,5 @@
-#Solution for the first problem of Google Code Jam 2019
-#Test 1 and 2 passed
 def prob1(n):
-    x1= round(n/4)
+    x1= round(n/2)
     x2= n-x1
     s1 = set(str(x1))
     s2 = set(str(x2))
@@ -11,15 +9,14 @@ def prob1(n):
             x2=x2+1111
             s1 = set(str(x1))
             s2 = set(str(x2))
-    else:  
+    else:
         while '4' in s1 or '4' in s2:
             x1=x1-1
             x2=x2+1
             s1 = set(str(x1))
             s2 = set(str(x2))
 
-    return x1, x2
-
+return x1, x2
 # input() reads a string with a line of input, stripping the ' ' (newline) at the end.
 # This is all you need for most Code Jam problems.
 t = int(input()) # read a line with a single integer
@@ -27,6 +24,5 @@ for i in range(1, t + 1):
     n = int(input())# read a list of integers, 2 in this case
     n1,n2 = prob1(n)
     print("Case #{}: {} {}".format(i, n1 , n2 ))
-      # check out .format's specification for more formatting options
-      
-     
+# check out .format's specification for more formatting options
+
